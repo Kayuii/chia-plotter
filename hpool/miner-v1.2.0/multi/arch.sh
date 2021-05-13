28219af408ef4b3f87cd9d9a750ae413af123b92
GEcko <577738@qq.com>
2021-05-13 23:13:52 +0800

    remove hpool miner

--- hpool/miner-v1.2.0/multi/arch.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-if [ `arch` = "x86_64" ] ; then ARCH=linux;fi
-if [ `arch` = "aarch64" ] ; then ARCH=aarch64;fi
-if [ `arch` = "armv7l" ] ; then ARCH=arm;fi
-VER=$1
-wget -q --no-check-certificate https://github.com/hpool-dev/chia-miner/releases/download/${VER}/HPool-Miner-chia-${VER}-${ARCH}.zip -O /tmp/chia-miner.zip && unzip -j /tmp/chia-miner.zip -d /tmp/linux
