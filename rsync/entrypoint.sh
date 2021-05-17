25b8fde152539e4f90600538ce1844b36a3f7cb5
Kayuii <577738@qq.com>
2021-05-17 11:28:39 +0800

    add rsync

--- rsync/entrypoint.sh
+++ rsync/entrypoint.sh
@@ -1,3 +1,17 @@
-#! /bin/sh
-plotfile=$(ls -l /mnt/dst/*.plot |awk 'NR==1{print $9}')
-echo "x $plotfile"
+#!/bin/sh
+plotfile=$(find /mnt/dst -type f -name "*.plot" |sort -n|head -1)
+if [ -z $plotfile ]; then
+echo "nofile, sleep 10m;"
+sleep 10m
+else
+echo $plotfile
+rsync --bwlimit=150000 $plotfile ubuntu@chia:/mnt/dst/001
+ret=$?
+if [ $ret -ne 0 ]; then
+  echo "rsync:failed: $ret"
+else
+  mkdir -p empty rmfile
+  mv $plotfile ./rmfile
+  rsync --delete-before -a -H --stats ./empty/ ./rmfile/
+fi
+fi
