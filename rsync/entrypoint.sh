47b1dea1772205d2d19d35a4756017da185265a4
Kayuii <577738@qq.com>
2021-05-17 17:38:30 +0800

    update rsync

--- rsync/entrypoint.sh
+++ rsync/entrypoint.sh
@@ -1,17 +1,61 @@
 #!/bin/sh
-plotfile=$(find /mnt/dst -type f -name "*.plot" |sort -n|head -1)
-if [ -z $plotfile ]; then
-echo "nofile, sleep 10m;"
-sleep 10m
-else
-echo $plotfile
-rsync --bwlimit=150000 $plotfile ubuntu@chia:/mnt/dst/001
-ret=$?
-if [ $ret -ne 0 ]; then
-  echo "rsync:failed: $ret"
-else
-  mkdir -p empty rmfile
-  mv $plotfile ./rmfile
-  rsync --delete-before -a -H --stats ./empty/ ./rmfile/
-fi
-fi
+
+APPNAME=rsync
+usage() {
+  if [ $# -eq 0 ]; then
+    echo ""
+    echo "${APPNAME}"
+    echo ""
+    echo "Usage:"
+    echo ""
+    printf "%s\n" "  ${APPNAME} <user:host> [path]"
+    exit 1
+  fi
+}
+
+rsync_run(){
+  [ "$#" -eq 2 ] || fatal "rsync faild: hostname and path."
+  local host="$1"
+  local path="$2"
+  plotfile=$(find /mnt/dst -type f -name "*.plot" |sort -n|head -1)
+  if [ -z $plotfile ]; then
+  echo "nofile, sleep 10m;"
+  sleep 10m
+  else
+  tmpfile=$(dirname $plotfile)
+  echo "$plotfile"
+  rsync --bwlimit=150000 $plotfile ${host}:${path}
+  ret=$?
+  if [ $ret -ne 0 ]; then
+    echo "rsync:failed: $ret"
+  else
+    mkdir -p ${tmpfile}/empty ${tmpfile}/rmfile
+    mv $plotfile ${tmpfile}/rmfile
+    rsync --delete-before -a -H --stats ${tmpfile}/empty/ ${tmpfile}/rmfile/
+  fi
+  fi
+}
+do_rsync() {
+  [ "$#" -eq 2 ] || fatal "faild: hostname and path."
+  local host="$1"
+  local path="$2"
+  rsync_run "${host}" "${path}"
+}
+
+
+[ "$#" -ge 3 ] || {
+  usage
+  exit 1
+}
+
+subcommand="$1"
+shift
+
+case "$subcommand" in
+rsync)
+  do_rsync "$@"
+  ;;
+*)
+  fatal "Unknown subcommand $subcommand"
+  ;;
+esac
